74926112febd89fc330bf0f87020eb09cba69c2d
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-05-01 17:35:00 +0200

    fix: path and command parse issues

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+//! main entry point
+
 #include "main.h"
 #include "doc/gen_html.h"
 #include "doc/parse.h"
@@ -6,11 +8,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define CMD_BUF 512
-
-#ifndef PATH_MAX
-#define PATH_MAX 4096
-#endif
+#define BUF 512
 
 void cmd_doc(char *src_dir, char *dest_dir, bool doc_open) {
     // initialize struct where doc tags will be accumulated
@@ -18,31 +16,31 @@ void cmd_doc(char *src_dir, char *dest_dir, bool doc_open) {
     DocItemList_init(&dil);
 
     // be sure gen folder exists
-    char mkdir_cmd[CMD_BUF];
+    char mkdir_cmd[BUF];
     snprintf(mkdir_cmd, sizeof(mkdir_cmd), "mkdir -p %s", src_dir);
     system(mkdir_cmd);
 
     // parse and generate
-    parse(dest_dir, dil);
-    gen_html(src_dir, dil);
+    parse(src_dir, dil);
+    gen_html(dest_dir, dil);
     // free
     DocItemList_free(dil);
 
     if (doc_open) {
-        char cmd[CMD_BUF * 4];
+        char cmd[BUF * 4];
         cmd[0] = '\0';
 #ifdef __APPLE__
-        strncat(cmd, "open ", CMD_BUF);
+        strncat(cmd, "open ", BUF);
 
 #elif defined(__linux__)
         strncat(cmd, "xdg-open ", size);
 
 #else
-        ERROR("Unknown or non-unix platform detected.\n");
+        ERROR("Unknown or non-unix platform detected\n");
 #endif
-        char path[CMD_BUF * 2];
-        snprintf(path, sizeof(path), "%s/index.html", src_dir);
-        strncat(cmd, path, CMD_BUF);
+        char path[BUF * 2];
+        snprintf(path, sizeof(path), "%s/index.html", dest_dir);
+        strncat(cmd, path, BUF);
         system(cmd);
     }
 }
@@ -66,7 +64,8 @@ int main(int argc, char *argv[]) {
                 open = 1;
                 break;
             default:
-                fprintf(stderr, "Usage: %s -s <src_dir> -d <dest_dir> [-o]\n", argv[0]);
+                fprintf(stderr, "%s -s <src_dir> -d <dest_dir> [-o: open in browser]\n",
+                        argv[0]);
                 return 1;
         }
     }
